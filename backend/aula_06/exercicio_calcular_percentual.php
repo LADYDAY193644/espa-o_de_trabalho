3a9a4a0f56f879c09c60e9f1c850a115206933d3
Seu Nome <seu_email@example.com>
2024-10-23 21:18:52 -0300

    exercicio_calcular_percentual.php

--- backend/aula_06/exercicio_calcular_percentual.php
+++ backend/aula_06/exercicio_calcular_percentual.php
@@ -1,6 +1,6 @@
 <?php
  $totalDeAulas = readline("Digite a quantidade de aulas frequentadas");
- $aulasMinistradas = readline("digite a quantidade de aulas ministradas")
+ $aulasMinistradas = readline("digite a quantidade de aulas ministradas");
 
 for($i = 1 ; $i  <= $aulasfrequentadas;$i++){
     $aulasfrequentadas = readline("Digite a quantidade de aulas frequentadas");
